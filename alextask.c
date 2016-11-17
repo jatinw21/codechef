a44fb370ebf5e58eb9782403e59234a799571501
Jatin wadhwa <jatinw21@gmail.com>
2016-11-18 04:04:34 +1100

    Final touches

--- alextask.c
+++ alextask.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <stdbool.h>
 #define LONG_MAX 9223372036854775807
 
-// by eucleadian theorem gcd(a,b)=gcd(b,r) where a =b*q +r , so a%b = r
-long long gcd(long long a, long long b) {
-    if(b == 0) {
-        return a;
-    } else {
-        return(gcd(b,a%b));
-    }
-}
+// function declarations
+long long gcd(long long a, long long b);
+long long lcm(long long a, long long b);
 
-long long lcm(long long a, long long b) {
-    return (a*b)/gcd(a,b);
-}
 
+// main function
 int main(int argc, char *argv[]) {
     int T, i, j, k, l, N;
     scanf("%d", &T);
@@ -25,7 +16,8 @@ int main(int argc, char *argv[]) {
 
     long long minLcm, m;
     // for each test cases
-    // get N then get all N's from next line, solve it
+    // get N then get all N's from next line
+    // solve it
     for (i = 0; i < T; i++) {
         scanf("%d", &N);
         // DEBUG
@@ -41,7 +33,9 @@ int main(int argc, char *argv[]) {
 
         // After getting all N's, now we do actual stuff
         // the smallest lcm of any two numbers is our solution.
-        minLcm = LONG_MAX;
+
+        minLcm = LONG_MAX; // largest number for first comparison
+
         for (k = 0; k < N-1; k++) {
             for (l = k+1; l < N; l++) {
                 m = lcm(times[k],times[l]);
@@ -53,5 +47,20 @@ int main(int argc, char *argv[]) {
         printf("%lld\n",minLcm);
 
     }
-    return 0;
+    return 0; // forgetting this gives an error on codechef.
+}
+
+// by eucleadian theorem gcd(a,b)=gcd(b,r) where a =b*q +r , so a%b = r
+// Using above, this function calculates the GCD
+long long gcd(long long a, long long b) {
+    if(b == 0) {
+        return a;
+    } else {
+        return(gcd(b,a%b));
+    }
+}
+
+// using GCD, this function calculates the LCM
+long long lcm(long long a, long long b) {
+    return (a*b)/gcd(a,b);
 }
