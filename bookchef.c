e8e4aa23f099582bc721c1056f0e8586e50a3a21
Jatin wadhwa <jatinw21@gmail.com>
2016-11-21 02:37:54 +1100

    All working and in very good time. Used structs to implement

--- bookchef.c
+++ bookchef.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 
 #define A_LARGE_NUMBER 1000000
-bool isSpecialFriend(int id, int numSpFriends, int spFriendIds[]);
+int isSpecialFriend(int id, int numSpFriends, int spFriendIds[]);
 typedef struct nodePosts {
-    bool special;
+    int special;
     int id;
     int popularity;
     char content[101];
@@ -50,41 +50,64 @@ int main(int argc, char const *argv[]) {
     }
 
     // for each post with special = 1, print most popularity one
-    // int max = 0;
-    // int j, jWithMax = 0;
-    // int prevMax = A_LARGE_NUMBER;
-    // for (i = 0; i < numPosts; i++) {
-    //     for (j = 0; j < numPosts; j++) {
-    //         if (posts[j].special == 1) {
-    //             if (posts[j].popularity > max && posts[j].popularity < prevMax) {
-    //                 max = posts[j].popularity;
-    //                 jWithMax = j;
-    //                 prevMax = max;
-    //             }
-    //         }
-    //     }
-    //     printf("%s\n", posts[j].content);
-    //     max = 0;
-    // }
-
-    int k;
-    for (k = 0; k < numPosts; k++) {
-        showStruct(posts[k]);
+    int max;
+    int flag1 = 1;
+    int flag2 = 1;
+    int reqdIndex;
+    while (flag1 || flag2) {
+        max = 0; flag1 = 0; flag2 = 0;
+        // printf("Going into 1st loop for checking if there is any special == 1\n");
+        for (i = 0; i < numPosts; i++) {
+            if (posts[i].special == 1) {
+                // printf("A special == 1 is found at %d\nFlag1 is set as 1\n",i );
+                flag1 = 1;
+                // printf("%d\n", flag1);
+                if (posts[i].popularity > max) {
+                    // printf("It's pop = %d is more than max = %d\n", posts[i].popularity, max);
+                    max = posts[i].popularity;
+                    // printf("so new max is pop\n");
+                    reqdIndex = i;
+                    // printf("Reqd Index is %d\n", i);
+                }
+            }
+        }
+        // printf("%d <- This is the value of flag1\nIf 1 should go in in statement.\n", flag1 );
+        if (flag1 == 1) {
+            // printf("Flag1 is 1 so printing the (reqdIndex)th post and setting its special to -1\n");
+            printf("%s\n", posts[reqdIndex].content);
+            // HERE IS THE PROBLEM
+            posts[reqdIndex].special = -1;
+            // printf("posts[%d].special is now %d\n",reqdIndex, posts[reqdIndex].special );
+        } else {
+            for (i = 0; i < numPosts; i++) {
+                if (posts[i].special == 0) {
+                    flag2 = 1;
+                    if (posts[i].popularity > max) {
+                        max = posts[i].popularity;
+                        reqdIndex = i;
+                    }
+                }
+            }
+            if (flag2 == 1) {
+                printf("%s\n", posts[reqdIndex].content);
+                posts[reqdIndex].special = -1;
+            }
+        }
     }
-
-
-
-
+    // int k;
+    // for (k = 0; k < numPosts; k++) {
+    //     showStruct(posts[k]);
+    // }
 
     return 0;
 }
 
-bool isSpecialFriend(int id, int numSpFriends, int spFriendIds[]) {
+int isSpecialFriend(int id, int numSpFriends, int spFriendIds[]) {
     int i;
     for (i = 0; i < numSpFriends; i++) {
-        if (id == spFriendIds[i]) return true;
+        if (id == spFriendIds[i]) return 1;
     }
-    return false;
+    return 0;
 }
 
 void showStruct(nodePosts k) {
